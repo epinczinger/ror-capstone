5007f9110152f29574272f952b66bb236442562c
EstebanP <epinczinger@gmail.com>
2020-10-21 12:37:47 -0300

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
   devise_for :users, :controllers => {registrations: 'registrations'}
+  resources :users, only: [:show, :index]
   resources :opinions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "opinions#index"
