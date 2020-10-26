a18d85950c605c4dcbcf84ec263ecdd394221120
EstebanP <epinczinger@gmail.com>
2020-10-25 22:36:56 -0300

    delete validation for deploy error

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,4 @@
 class Following < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
-  validates :followed_id, presence: true
-  validates :follower_id, presence: true
 end
