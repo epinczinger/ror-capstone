49f3a881d852dd6bd4aaf797fd9542f05afa993a
EstebanP <epinczinger@gmail.com>
2020-10-27 09:06:55 -0300

    delete duplicated routes on users

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-   
-  resources :opinions do
-    resources :likes, only: [:create]
-  end
-  delete 'like', to: 'likes#destroy'
-  resources :followings
+  
 
-  
-  get 'users/show'
-  get 'users/index'
-  
   authenticate :user do
   root to: 'opinions#index', as: :authenticated_root
   end
   root to: redirect('/users/sign_in')
-
-
+  
+  resources :opinions do
+    resources :likes, only: [:create]
+  end  
+  
+  delete 'like', to: 'likes#destroy'
+  resources :followings
+  
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: %i[show index]
-
- 
-
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
